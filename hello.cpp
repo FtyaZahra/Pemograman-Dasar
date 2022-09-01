a326ec0f0a3e12409d8639da6113a03c39071a08
FtyaZahra <111962292+FtyaZahra@users.noreply.github.com>
2022-09-01 14:59:09 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+  
+//Compiler version g++ 6.3.0
+
+int main() 
+{
+    cout << "nama:FtyaZahra"<< endl;
+    cout << "kelas:Xpplg1"<<endl;
+    cout << "========="<<endl;
+    cout << " angka1 =10"<<endl;
+    cout << "angka2 =5"<<endl;
+    cout << "hasil:"<<10+5 <<endl;
+    
+     return 0;
+}
